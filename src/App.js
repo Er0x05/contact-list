9f3ae2217a088f5043823d6e794d4f9d3f27408c
pek <pek.workit@gmail.com>
2019-10-29 04:40:10 +0800

    add func search n remove item

--- src/App.js
+++ src/App.js
@@ -2,32 +2,44 @@ import React, { Component } from 'react';
 
 import ListContacts from './ListContacts'
 
-const contacts = [
- {
-   "id": "karen",
-   "name": "Karen Isgrigg",
-   "handle": "karen_isgrigg",
-   "avatarURL": "http://localhost:5001/karen.jpg"
- },
- {
-   "id": "richard",
-   "name": "Richard Kalehoff",
-   "handle": "richardkalehoff",
-   "avatarURL": "http://localhost:5001/richard.jpg"
- },
- {
-   "id": "tyler",
-   "name": "Tyler McGinnis",
-   "handle": "tylermcginnis",
-   "avatarURL": "http://localhost:5001/tyler.jpg"
- }
-];
-
 class App extends Component {
+
+  state = {
+    contacts: [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+
+  removeContact = ( contact ) => {
+    this.setState( currentState => ({
+      contacts: currentState.contacts.filter(
+        currentContact => currentContact.id !== contact.id )
+    }))
+  }
+  
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts}/>
+        <ListContacts 
+          contacts={this.state.contacts}
+          remove={this.removeContact}/>
       </div>
     );
   }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,24 +1,75 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 class ListContacts extends React.Component {
 
+	static propTypes = {
+		contacts: PropTypes.array.isRequired,
+		remove: PropTypes.func.isRequired
+	}
+
+	state = {
+		query: ''
+	}
+
+	updateQuery = (e) => {
+		this.setState( () => ({ query: e.trim() }) )
+	}
+
+	clearQuery = () => {
+		this.updateQuery('')
+	}
+	
 	render(){
-		console.log('props',this.props)
+
+		const { query } = this.state;
+		const { contacts, remove } = this.props;
+
+		const showingContact = query === ''
+			? contacts 
+			: contacts.filter( e => (
+				e.name.toLowerCase().includes(query.toLocaleLowerCase())
+			) )
+		
 		return(
-			<ol className='contact-list'>
-				{this.props.contacts.map((contact)=>(
-					<li key={contact.id} className='contact-list-item'>
-						<div className='contact-avatar' style={{
-							backgroundImage: `url(${contact.avatarURL})`
-						}}></div>
-						<div className='contact-details'>
-							<p>{contact.name}</p>
-							<p>@{contact.handle}</p>
-						</div>
-						<button className='contact-remove'>Remove</button>
-					</li>
-				))}
-			</ol>
+			<div className='list-contacts'>
+				<div className='list-contacts-top'>
+					<input 
+						className='search-contacts' 
+						type='text'
+						placeholder='Search Contacts'
+						value={this.state.query}
+						onChange={ e => this.updateQuery(e.target.value)}
+					/>
+				</div>
+
+				{ showingContact.length !== contacts.length && (
+					<div className='showing-contacts'>
+						<span>Now showing {showingContact.length} of {contacts.length}</span>
+						<button onClick={this.clearQuery}>Show all</button>
+					</div>
+				)}
+
+				{/* {JSON.stringify(this.props)} */}
+				<ol className='contact-list'>
+					{showingContact.map((contact)=>(
+						<li key={contact.id} className='contact-list-item'>
+							<div className='contact-avatar' style={{
+								backgroundImage: `url(${contact.avatarURL})`
+							}}></div>
+							<div className='contact-details'>
+								<p>{contact.name}</p>
+								<p>@{contact.handle}</p>
+							</div>
+							<button 
+								className='contact-remove'
+								onClick={ ()=>remove(contact) }
+								>Remove</button>
+						</li>
+					))}
+				</ol>	
+			</div>
 		)
 	}
 }
